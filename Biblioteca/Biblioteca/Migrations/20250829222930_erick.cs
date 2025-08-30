137fac9ba78627eff41641103e7b2f75c6525856
ERICKBORGESRODRIGUES <ERICKBORGESRODRIGUES@SCS217791>
2025-08-29 21:59:01 -0300

    Dia 29/08 - Verificando se salvou

---
+++ Biblioteca/Biblioteca/Migrations/20250829222930_erick.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Migrations
+{
+    /// <inheritdoc />
+    public partial class erick : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Livros",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emprestimos_AlunoId",
+                table: "Emprestimos",
+                column: "AlunoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Emprestimos_Alunos_AlunoId",
+                table: "Emprestimos",
+                column: "AlunoId",
+                principalTable: "Alunos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Emprestimos_Alunos_AlunoId",
+                table: "Emprestimos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Emprestimos_AlunoId",
+                table: "Emprestimos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Titulo",
+                table: "Livros",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
